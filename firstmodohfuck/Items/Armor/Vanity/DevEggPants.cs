e95028252f5ce3119ffdcdde9981f879b537f3b6
FirstModFolley <62345946+FirstModFolley@users.noreply.github.com>
2020-03-18 11:30:47 -0400

    Add files via upload

---
+++ firstmodohfuck/Items/Armor/Vanity/DevEggPants.cs
@@ -0,0 +1,22 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace firstmodohfuck.Items.Armor.Vanity
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class DevEggPants : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Egg's Pants");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 22;
+            item.height = 18;
+            item.vanity = true;
+            item.rare = -12;
+        }
+    }
+}
